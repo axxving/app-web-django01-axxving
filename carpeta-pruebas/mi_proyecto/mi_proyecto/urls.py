f9829dfd6cb0082ed0c019ef14517153b5e146f3
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-06 11:47:44 -0600

    feat(): first hello work
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- carpeta-pruebas/mi_proyecto/mi_proyecto/urls.py
+++ carpeta-pruebas/mi_proyecto/mi_proyecto/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Importar app con mis vistas
+from miapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hola-work/', views.hola_mundo, name="hola_mundo")
 ]
--- carpeta-pruebas/mi_proyecto/miapp/views.py
+++ carpeta-pruebas/mi_proyecto/miapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+# MVC = Modelo vista controlador -> Acciones (metodos)
+# MVT = Modelo vista template -> (metodos)
+
+def hola_mundo (request):
+    return HttpResponse("<h1>Hello work with Django</h1>")
